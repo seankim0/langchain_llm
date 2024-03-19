695d78b6e4c7c554c3c03aad28a2f5d37b137c5a
seankim0 <seunghyk@tepper.cmu.edu>
2024-03-19 00:03:06 -0700

    testing this feature

--- RestaurantNameGenerator/main.py
+++ RestaurantNameGenerator/main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import langchain_helper
 
-st.title("Restaurant Name Generator")
+st.title("Supplier Main Products and Leed Times by Country")
 
-cuisine = st.sidebar.selectbox("Pick a Cuisine", ("Indian", "Italian", "Mexican", "Arabic", "American", "Chinese", "Korean"))
+cuisine = st.sidebar.selectbox("Pick a Country", ("United States", "China", "Mexico", "South East Asia", "Eastern Europe", "Middle East and Africa", "India"))
 
 if cuisine:
     response = langchain_helper.generate_restaurant_name_and_items(cuisine)
